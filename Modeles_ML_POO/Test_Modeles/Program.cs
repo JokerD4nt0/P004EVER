c8ec4d09bd85d6f380ee19bdcead4cee4bed7bd5
Jean-Sylvain Boige <jsboige@gmail.com>
2019-08-22 11:11:02 +0200

    Implémentation de la méthode Predict

---
+++ Modeles_ML_POO/Test_Modeles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Modeles_ML_POO;
+using NumSharp;
+
+namespace Test_Modeles
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var lrm = new LinearRegressionModel {Weights = np.array(new double[] {2, 4, 6, 2, 2})};
+			var x = np.array(new double[][] {new double[] {1, 1, 1, 1}});
+			
+			//Transformation de X depuis une matrice vers une collection de collection d'objets
+			var array = x.ToJaggedArray<double>();
+			var list = array.Cast<double[]>().Select(point=>new List<object>(point.Cast<object>())).Cast<ICollection<object>>().ToArray();
+
+			var resultat = lrm.Predict(list);
+			Console.WriteLine(resultat.ToString());
+			Console.Read();
+		}
+	}
+}
